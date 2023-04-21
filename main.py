0e4e4dab409859d41f68249c61004cc6732ce276
marquezlianna <129668304+marquezlianna@users.noreply.github.com>
2023-04-21 09:23:54 +0800

    encrypt the text

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ def generateKey(string, key):
             key.append(key[i % len(key)])
     return ("".join(key))
 
+# encrypt the text
+def cipherText(string, key):
+    cipher_text = []
+    for i in range(len(string)):
+        x = (ord(string[i]) + ord(key[i])) % 26
+        x += ord('A')
+        cipher_text.append(chr(x))
+    return ("".join(cipher_text))
