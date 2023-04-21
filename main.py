f82deb94403eb7b5cc71b1f11ee46e027fd08cd4
marquezlianna <129668304+marquezlianna@users.noreply.github.com>
2023-04-21 09:14:33 +0800

    generate key

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 # import pyfiglet module
 import pyfiglet
+
+# generate key
+def generateKey(string, key):
+    key = list(key)
+    if len(string) == len(key):
+        return (key)
+    else:
+        for i in range(len(string) -
+                       len(key)):
+            key.append(key[i % len(key)])
+    return ("".join(key))
+
